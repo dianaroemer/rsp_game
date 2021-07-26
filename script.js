54490b2dd7d8f11dc862ed4209f5b28ab1463c91
dominicroemer <dominicroemer@gmail.com>
2021-07-26 15:49:14 -0700

    Add function computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+
+
+
+function computerPlay() {
+
+    //Generate a random number between 1 and 3 using the Math.random() function
+    let num = (Math.floor(Math.random() * 3) + 1) ;
+
+    //Decision tree to determine return output based on num
+    if( num === 1 ) {
+        return 'Rock';
+    } else if ( num === 2 ) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+
+    throw "Function computerPlay() reached unknown value!";
+
+}
